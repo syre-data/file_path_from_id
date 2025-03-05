7d6e74c4c35e5bcb33e6378cf5badf2be6059371
Brian Carlsen <carlsen.bri@gmail.com>
2025-03-05 19:02:14 +0100

    Fixed bugs in examples/basic.

--- examples/basic/main.rs
+++ examples/basic/main.rs
@@ -1,6 +1,6 @@
 use file_path_from_id::path_from_id;
-use notify::{RecursiveMode, Watcher};
-use notify_debouncer_full::{DebounceEventResult, FileIdCache};
+use notify::RecursiveMode;
+use notify_debouncer_full::DebounceEventResult;
 use std::fs;
 use std::sync::mpsc;
 use std::time::Duration;
@@ -22,56 +22,39 @@ fn main() {
 
     let watch_path = fs::canonicalize(WATCH_PATH).unwrap();
     watcher
-        .watcher()
         .watch(&watch_path, RecursiveMode::Recursive)
         .unwrap();
-
-    watcher
-        .cache()
-        .add_root(&watch_path, RecursiveMode::Recursive);
 
     loop {
         match rx.recv().unwrap() {
             Ok(events) => {
                 for event in events {
-                    let cache = watcher.cache();
-
-                    println!("{cache:?}\n");
                     println!("{event:?}\n");
                     match &event.paths[..] {
                         [path] => {
-                            let Some(info) = cache.cached_file_id(&path) else {
-                                println!("path not cached\n");
+                            let Ok(info) = file_id::get_file_id(path) else {
                                 continue;
                             };
 
-                            match path_from_id(info) {
+                            match path_from_id(&info) {
                                 Ok(path) => println!("found {path:?}\n"),
                                 Err(err) => println!("err {err:?}\n"),
                             }
                         }
 
                         [from, to] => {
-                            match cache.cached_file_id(&from) {
-                                None => {
-                                    println!("from path not cached\n");
-                                }
-
-                                Some(info) => match path_from_id(info) {
+                            if let Ok(info) = file_id::get_file_id(from) {
+                                match path_from_id(&info) {
                                     Ok(path) => println!("from {path:?}\n"),
                                     Err(err) => println!("from err {err:?}\n"),
-                                },
-                            }
-
-                            match cache.cached_file_id(&to) {
-                                None => {
-                                    println!("to path not cached\n");
                                 }
+                            };
 
-                                Some(info) => match path_from_id(info) {
+                            if let Ok(info) = file_id::get_file_id(to) {
+                                match path_from_id(&info) {
                                     Ok(path) => println!("to {path:?}\n"),
                                     Err(err) => println!("to err {err:?}\n"),
-                                },
+                                }
                             }
                         }
 
